350af25a745a3df39ee8b9aa66931fad073342f2
Kacper <rybkowski84@gmail.com>
2023-11-16 23:15:43 +0100

    Dodaj pliki projektów.

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include<GLFW/glfw3.h>
+
+const unsigned int height = 800;
+const unsigned int width = 800;
+
+int main()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(width, height, "window", NULL, NULL);
+
+	glfwMakeContextCurrent(window);
+
+
+	glEnable(GL_DEPTH_TEST);
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
