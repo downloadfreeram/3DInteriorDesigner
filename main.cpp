2f806d0c978fa8854a5daeba622c2dd92b0e06c1
Kacper <rybkowski84@gmail.com>
2023-12-10 19:00:02 +0100

    10.12.2023 //fixed

--- main.cpp
+++ main.cpp
@@ -1,117 +1,257 @@
-#include"imgui.h"
-#include"imgui_impl_glfw.h"
-#include"imgui_impl_opengl3.h"
-#include"imgui_impl_opengl3_loader.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
-#include<GLFW/glfw3.h>
-#include<iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-#include"Model.h"
-#include"Shader.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-const unsigned int height = 800;
-const unsigned int width = 800;
+#include "Model.h"
+#include "Camera.h"
+#include "Shader.h"
 
-bool showTestWindow = true;
-bool showTest2Window = false;
-bool showTest3Window = false;
+#include <iostream>
 
-void jed();
-void dwa();
-void trzy();
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
 
-void jed()
-{
-	ImGui::Begin("Test", &showTestWindow);
-	ImGui::Text("Lorem ipsum");
-	if (ImGui::Button("Next"))
-	{
-		showTestWindow = false;
-		showTest2Window = true;
-	}
-	if (ImGui::Button("Next"))
-	{
-		showTestWindow = false;
-		showTest3Window = true;
-	}
-	ImGui::End();
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+bool showMainMenu = true;
+bool showSecondaryWindow = false;
+bool showModelWindow = false;
+
+glm::vec3 posXYZ = glm::vec3(0.0f, 0.0f, 0.0f);
+float rot = 0.0f;
+
+void DisplaySecondaryWindow() {
+    showMainMenu = false;
+    showSecondaryWindow = true;
 }
 
-void dwa() 
-{
-	ImGui::Begin("Test 2", &showTest2Window);
-	ImGui::Text("Lorem Ipsum");
-	if (ImGui::Button("Back"))
-	{
-		showTest2Window = false;
-		showTestWindow = true;
-	}
-	ImGui::End();
+void DisplayModelWindow() {
+    showMainMenu = false;
+    showModelWindow = true;
 }
-void trzy() 
-{
-	ImGui::Begin("Test 3", &showTest3Window);
-	ImGui::Text("Lorem Ipsum");
-	if (ImGui::Button("Back"))
-	{
-		showTest3Window = false;
-		showTestWindow = true;
-	}
-	ImGui::End();
+
+void MainMenu() {
+    ImGui::Begin("Main Menu", &showMainMenu);
+
+    if (ImGui::Button("Open Secondary Window")) {
+        DisplaySecondaryWindow();
+    }
+
+    if (ImGui::Button("Load Model")) {
+        DisplayModelWindow();
+    }
+
+    ImGui::End();
 }
+
+void SecondaryWindow() {
+    ImGui::Begin("Secondary Window", &showSecondaryWindow);
+
+    if (ImGui::Button("Back to Main Menu")) {
+        showSecondaryWindow = false;
+        showMainMenu = true;
+    }
+
+    ImGui::End();
+}
+void RenderModelWindow(GLFWwindow* window, Shader& ourShader, Model& ourModel) {
+    ImGui::Begin("Loaded Model", &showModelWindow);
+    ImGui::SliderFloat3("Position", glm::value_ptr(posXYZ), 0.0f, 10.0f);
+    ImGui::SliderFloat("Rotation", &rot, 0.0f, 360.0f);
+
+    //per-frame time logic
+    float currentFrame = static_cast<float>(glfwGetTime());
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+
+    processInput(window);
+
+    //enable shader 
+    ourShader.use();
+
+    // view/projection transformations
+    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), static_cast<float>(SCR_WIDTH) / static_cast<float>(SCR_HEIGHT), 0.1f, 100.0f);
+    glm::mat4 view = camera.GetViewMatrix();
+    ourShader.setMat4("projection", projection);
+    ourShader.setMat4("view", view);
+
+    // render the loaded model
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, posXYZ);
+    model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+
+    model = glm::rotate(model, glm::radians(rot), glm::vec3(0.0f, 1.0f, 0.0f));
+
+    ourShader.setMat4("model", model);
+    ourModel.Draw(ourShader);
+
+    ImGui::End();
+}
+
 int main()
 {
-	glfwInit();
+    // glfw: initialize and configure
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // flip on y-axis
+    stbi_set_flip_vertically_on_load(true);
+
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile shaders
+    Shader ourShader("default.vert", "default.frag");
+
+    // load models
+    Model ourModel("resources/objects/untitled.obj");
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
 
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // render
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(width, height, "window", NULL, NULL);
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
 
-	glfwMakeContextCurrent(window);
 
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	ImGui::StyleColorsDark();
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 330");
+        if (showMainMenu) {
+            MainMenu();
+        }
 
-	glEnable(GL_DEPTH_TEST);
-	while (!glfwWindowShouldClose(window))
-	{
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
+        if (showSecondaryWindow) {
+            SecondaryWindow();
+        }
 
-		if (showTestWindow)
-		{
-			jed();
-		}
-		else if (showTest2Window)
-		{
-			dwa();
-		}
-		else if (showTest3Window)
-		{
-			trzy();
-		}
+        if (showModelWindow) {
+            RenderModelWindow(window, ourShader, ourModel);
+        }
 
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	return 0;
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    glfwTerminate();
+    return 0;
 }
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
